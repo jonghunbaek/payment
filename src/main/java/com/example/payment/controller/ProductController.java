ffadddb7fb94b02397f56f79d02a2dc3caa98e98
jonghunbaek <qor6028@naver.com>
2024-01-22 19:08:20 +0900

    refactor : iamport 식별 코드 서버에서 내려 받도록 변경 및 결제 창 노출 성공

--- src/main/java/com/example/payment/controller/ProductController.java
+++ src/main/java/com/example/payment/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.example.payment.controller.dto.ProductResponse;
 import com.example.payment.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,11 +17,16 @@ import java.util.List;
 public class ProductController {
 
     private final ProductService productService;
+    @Value("${imp.code}")
+    private String impCode;
 
     @GetMapping("/products")
     public String findAll(Model model) {
         List<ProductResponse> products = productService.findAllProducts();
+
         model.addAttribute("products", products);
+        model.addAttribute("impCode", impCode);
+
         return "product";
     }
 }
