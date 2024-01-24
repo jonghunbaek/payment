cf9431238cee5df3886070cd37af9e5b676a195c
jonghunbaek <qor6028@naver.com>
2024-01-24 19:42:56 +0900

    feat : 결제 성공 로직 작성 완료

--- src/main/java/com/example/payment/controller/dto/PaymentResult.java
+++ src/main/java/com/example/payment/controller/dto/PaymentResult.java
@@ -1,7 +1,36 @@
 package com.example.payment.controller.dto;
 
+import com.siot.IamportRestClient.response.Payment;
+import lombok.Builder;
 import lombok.Getter;
+
+import java.math.BigDecimal;
 
 @Getter
 public class PaymentResult {
+
+    String imp_uid;
+    String merchant_uid;
+    String name;
+    BigDecimal amount;
+    String status;
+
+    @Builder
+    private PaymentResult(String imp_uid, String merchant_uid, String name, BigDecimal amount, String status) {
+        this.imp_uid = imp_uid;
+        this.merchant_uid = merchant_uid;
+        this.name = name;
+        this.amount = amount;
+        this.status = status;
+    }
+
+    public static PaymentResult of(Payment payment) {
+        return PaymentResult.builder()
+            .imp_uid(payment.getImpUid())
+            .merchant_uid(payment.getMerchantUid())
+            .name(payment.getName())
+            .amount(payment.getAmount())
+            .status(payment.getStatus())
+            .build();
+    }
 }
--- src/main/java/com/example/payment/service/PaymentService.java
+++ src/main/java/com/example/payment/service/PaymentService.java
@@ -8,9 +8,11 @@ import com.siot.IamportRestClient.response.IamportResponse;
 import com.siot.IamportRestClient.response.Payment;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
 
+@Transactional
 @RequiredArgsConstructor
 @Service
 public class PaymentService {
@@ -18,19 +20,25 @@ public class PaymentService {
     private final IamportClient iamportClient;
 
     public PaymentResult createPayment(PaymentInfo paymentInfo) {
+        IamportResponse<Payment> impResponse;
+
         try {
-            IamportResponse<Payment> impResponse = iamportClient.paymentByImpUid(paymentInfo.getImpUid());
-            boolean isSameAmount = impResponse.getResponse().getAmount().toString().equals(paymentInfo.getPaidAmount());
-
-            if (isSameAmount) {
-                return null;
-            }
-
-            // 환불 로직
+            impResponse = iamportClient.paymentByImpUid(paymentInfo.getImpUid());
         } catch (IamportResponseException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+
+        validateAmount(paymentInfo, impResponse);
+
+        return PaymentResult.of(impResponse.getResponse());
+    }
+
+    private void validateAmount(PaymentInfo paymentInfo, IamportResponse<Payment> impResponse) {
+        if (!impResponse.getResponse().getAmount().toString().equals(paymentInfo.getPaidAmount())) {
+            throw new IllegalStateException("결제 금액이 일치하지 않습니다. realAmount, requestAmount :: "
+                + impResponse.getResponse().getAmount() + ", " + paymentInfo.getPaidAmount());
         }
     }
 }
