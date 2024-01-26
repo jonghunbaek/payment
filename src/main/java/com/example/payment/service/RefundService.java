6e3b39344c6543b293c4e26d4ec40d877296c3b7
jonghunbaek <qor6028@naver.com>
2024-01-26 17:08:51 +0900

    feat : 결제 환불 로직 작성

--- src/main/java/com/example/payment/service/RefundService.java
+++ src/main/java/com/example/payment/service/RefundService.java
@@ -1,10 +1,18 @@
 package com.example.payment.service;
 
 import com.example.payment.controller.dto.PaymentResult;
+import com.example.payment.service.dto.PaymentInfo;
 import com.siot.IamportRestClient.IamportClient;
+import com.siot.IamportRestClient.exception.IamportResponseException;
+import com.siot.IamportRestClient.request.CancelData;
+import com.siot.IamportRestClient.response.IamportResponse;
+import com.siot.IamportRestClient.response.Payment;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
+import java.math.BigDecimal;
 
 @Transactional
 @RequiredArgsConstructor
@@ -13,7 +21,19 @@ public class RefundService {
 
     private final IamportClient iamportClient;
 
-    public PaymentResult refund() {
+    public PaymentResult refund(PaymentInfo paymentInfo) {
+        BigDecimal amount = BigDecimal.valueOf(Long.valueOf(paymentInfo.getPaidAmount()));
+        CancelData cancelData = new CancelData(paymentInfo.getImpUid(), true, amount);
+
+        IamportResponse<Payment> cancelResult;
+        try {
+             cancelResult = iamportClient.cancelPaymentByImpUid(cancelData);
+        } catch (IamportResponseException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         return null;
     }
 }
